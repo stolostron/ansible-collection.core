d83cf9767b862ccc0f9ec6caefdddaa46cf6219e
Tsu Phin Hee <thee@redhat.com>
2022-01-21 17:30:36 -0500

    add wait for proxy url available
    Signed-off-by: Tsu Phin Hee <thee@redhat.com>

--- plugins/modules/cluster_proxy_addon.py
+++ plugins/modules/cluster_proxy_addon.py
@@ -60,6 +60,8 @@ err:
 '''
 
 import traceback
+import requests
+import urllib3
 
 from ansible.module_utils.basic import AnsibleModule, env_fallback, missing_required_lib
 from ansible_collections.ocmplus.cm.plugins.module_utils.import_utils import get_managed_cluster
@@ -113,6 +115,22 @@ def get_hub_proxy_route(hub_client, ocm_namespace: str):
     except NotFoundError:
         return None
     return route.spec.host
+
+
+def wait_for_proxy_route_available(url, timeout=60):
+    max_retry = 5
+    retries = urllib3.util.retry.Retry(total=max_retry,
+                                       backoff_factor=timeout /
+                                       max_retry / (max_retry + 1),
+                                       status_forcelist=[500, 502, 503, 504])
+    session = requests.Session()
+    session.mount(
+        'https://', requests.adapters.HTTPAdapter(max_retries=retries))
+    try:
+        session.get(url, verify=False)
+        return True
+    except requests.exceptions.RetryError as e:
+        return False
 
 
 def get_ocm_install_namespace(hub_client):
@@ -170,6 +188,12 @@ def execute_module(module: AnsibleModule):
                          err="failed to get hub proxy url")
 
     cluster_url = f"https://{hub_proxy_url}/{managed_cluster_name}"
+    health_url = f"{cluster_url}/healthz"
+    if wait:
+        if not wait_for_proxy_route_available(health_url, timeout):
+            module.fail_json(f"timed out waiting for proxy url {health_url} to become available",
+                             err=f"timed out waiting for proxy url {health_url} to become available")
+
     module.exit_json(cluster_url=cluster_url)
 
 
