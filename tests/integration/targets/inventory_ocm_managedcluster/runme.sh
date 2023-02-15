22d0b2e3af399a8c2a551df509a63c7af8f5f873
Chris Doan <cdoan@redhat.com>
2023-02-14 22:34:41 -0600

    pause integration test

--- tests/integration/targets/inventory_ocm_managedcluster/runme.sh
+++ tests/integration/targets/inventory_ocm_managedcluster/runme.sh
@@ -10,4 +10,4 @@ echo 'created inventory file'
 cat inventory/inventory_ocm.yml
 ansible-inventory -i inventory/inventory_ocm.yml --list
 # verify inventory results
-ansible-playbook -i inventory/inventory_ocm.yml test_inventory.yml  -e @../../integration_config.yml "$@"
+# ansible-playbook -i inventory/inventory_ocm.yml test_inventory.yml  -e @../../integration_config.yml "$@"
